ac5cf33f70414f26efe0c257dbed89c2d7a9255e
Ben Krikler <bek07@ic.ac.uk>
2018-11-05 23:52:23 +0100

    General changes to main file

--- skhep_testdata/__init__.py
+++ skhep_testdata/__init__.py
@@ -4,22 +4,21 @@ from . import remote_files
 
 
 __all__ = ["data_path"]
-
-
 __top_directory__ = os.path.realpath(os.path.dirname(__file__))
+__data_directory__ = os.path.join(__top_directory__, "data")
 
 
 def data_path(filename, raise_missing=False):
     if remote_files.is_known_remote(filename):
         return remote_files.remote_file(filename, raise_missing)
 
-    path = os.path.join(__top_directory__, filename)
+    path = os.path.join(__data_directory__, filename)
     if not os.path.isfile(path) and raise_missing:
         raise RuntimeError("%s cannot be found" % filename)
     return path
 
 
 if __name__ == "__main__":
-    print("BEK __top_directory__", __top_directory__)
-    print("BEK", data_path("hello.root"))
-    print("BEK", data_path("hello.root", raise_missing=True))
+    print("__top_directory__", __top_directory__)
+    print(data_path("hello.root"))
+    print(data_path("hello.root", raise_missing=True))
