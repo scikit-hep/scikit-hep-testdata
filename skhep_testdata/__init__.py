40aed9aeeb5d6bab444449456e8a763d0e588069
eduardo-rodrigues <eduardo.rodrigues@cern.ch>
2019-02-26 11:26:31 +0100

    Simplification of version number setting

--- skhep_testdata/__init__.py
+++ skhep_testdata/__init__.py
@@ -1,6 +1,9 @@
 from __future__ import absolute_import
+
 from .local_files import data_path
 
 
 __all__ = ["data_path"]
-__version__ = '0.1.1'
+
+# Convenient access to the version number
+from .version import __version__
---
+++ skhep_testdata/version.py
@@ -0,0 +1,4 @@
+__version__ = '0.1.1'
+
+version = __version__
+version_info = __version__.split('.')
