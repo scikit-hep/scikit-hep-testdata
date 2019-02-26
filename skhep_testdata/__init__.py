1599bf6a5ecf887706ea041950af14250bc31a6b
eduardo-rodrigues <eduardo.rodrigues@cern.ch>
2019-02-26 11:43:58 +0100

    Please PEP8

--- skhep_testdata/__init__.py
+++ skhep_testdata/__init__.py
@@ -2,8 +2,7 @@ from __future__ import absolute_import
 
 from .local_files import data_path
 
-
-__all__ = ["data_path"]
-
 # Convenient access to the version number
 from .version import __version__
+
+__all__ = ["data_path"]
