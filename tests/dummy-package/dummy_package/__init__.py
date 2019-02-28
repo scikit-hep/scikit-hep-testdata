436ec5c14ed3bcdae9231163fda2d109464aeefc
Ben Krikler <bek07@ic.ac.uk>
2019-02-28 19:07:57 +0100

    Add some dbugging to look into the travis pipeline

--- tests/dummy-package/dummy_package/__init__.py
+++ tests/dummy-package/dummy_package/__init__.py
@@ -1,6 +1,22 @@
+from __future__ import print_function
+import os
 from skhep_testdata import data_path
 
 
 def run():
     filename = data_path("uproot-Event.root")
     return filename
+
+
+def debug():
+    import skhep_testdata.remote_files as remote
+    print("_remote_dataset_cfg", remote._remote_dataset_cfg)
+
+    for root, dirs, files in os.walk(remote._default_data_dir, topdown=False):
+        for name in files:
+            print(os.path.join(root, name))
+        for name in dirs:
+            print(os.path.join(root, name))
+
+if __name__ == "__main__":
+    debug()
