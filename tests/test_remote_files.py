ee51f1003c2a9a787c0ece82d6babdcae0143717
eduardo-rodrigues <eduardo.rodrigues@cern.ch>
2019-04-23 11:15:05 +0200

    Fix tests on Windows

--- tests/test_remote_files.py
+++ tests/test_remote_files.py
@@ -6,13 +6,17 @@ _remote_dataset_cfg = os.path.join(os.path.dirname(__file__), "test_remote_datas
 skhtd.remote_files._remote_dataset_cfg = _remote_dataset_cfg
 
 
+good_file_1 = os.path.join("dataset_1", "file_1.root")
+bad_file_1 = os.path.join("bad_dataset_1", "file_1.root")
+
+
 def test_is_known_remote():
-    assert skhtd.remote_files.is_known_remote("dataset_1/file_1.root")
-    assert not skhtd.remote_files.is_known_remote("bad_dataset_1/file_1.root")
+    assert skhtd.remote_files.is_known_remote(good_file_1)
+    assert not skhtd.remote_files.is_known_remote(bad_file_1)
 
 
 def test_get_config_for_file():
-    config = skhtd.remote_files.RemoteDatasetList.get_config_for_file("dataset_1/file_1.root")
+    config = skhtd.remote_files.RemoteDatasetList.get_config_for_file(good_file_1)
     assert config["dataset_name"] == "dataset_1"
     assert config["url"] == "http://not-a-real-url.com/dataset_1.tar"
 
@@ -31,5 +35,5 @@ def test_remote_file(tmpdir, monkeypatch):
             outfile.write("testing...")
     monkeypatch.setattr(skhtd.remote_files, "urlretrieve", fake_urlretrieve)
 
-    path = skhtd.remote_files.remote_file("dataset_1/file_1.root", data_dir=str(tmpdir))
+    path = skhtd.remote_files.remote_file(good_file_1, data_dir=str(tmpdir))
     assert path == str(tmpdir / "dataset_1" / "file_1.root")
