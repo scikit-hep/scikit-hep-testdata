9a0f6cc426e86d4227fbc52618ecc26c0db11d3b
Henry Schreiner <henry.fredrick.schreiner@cern.ch>
2020-03-06 15:30:16 -0500

    Work with importlib_resources 1.3 released an hour ago (#27)

--- src/skhep_testdata/local_files.py
+++ src/skhep_testdata/local_files.py
@@ -9,6 +9,11 @@ from . import remote_files
 import importlib_resources
 
 try:
+    from importlib_resources import as_file
+except ImportError:
+    from importlib_resources.trees import as_file
+
+try:
     from contextlib import ExitStack
 except ImportError:
     from contextlib2 import ExitStack
@@ -23,7 +28,7 @@ def data_path(filename, raise_missing=True):
     ref = importlib_resources.files("skhep_testdata.data") / filename
     file_manager = ExitStack()
     atexit.register(file_manager.close)
-    file_path = file_manager.enter_context(importlib_resources.trees.as_file(ref))
+    file_path = file_manager.enter_context(as_file(ref))
     if raise_missing and not file_path.exists():
             raise RuntimeError("Unknown or missing file: {0}".format(filename))
     return str(file_path)
