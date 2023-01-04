3a1f27a7f504efc728dff99d4e0951c83f7226a6
Eduardo Rodrigues <eduardo.rodrigues@cern.ch>
2023-01-04 12:31:29 +0100

    Fixes for mypy

--- src/skhep_testdata/local_files.py
+++ src/skhep_testdata/local_files.py
@@ -26,8 +26,7 @@ with resources.as_file(resources.files(data) / "file_list.txt") as fp, fp.open()
     known_files = {n.strip() for n in f if n.strip()}
 
 
-def _cache_path(cache_dir=None):
-    # type: (Optional[str]) -> Path
+def _cache_path(cache_dir: Optional[str] = None) -> Path:
     if cache_dir is None:
         skhepdir = Path.home() / ".local" / "skhepdata"
         skhepdir.mkdir(exist_ok=True, parents=True)
@@ -36,8 +35,9 @@ def _cache_path(cache_dir=None):
         return Path(cache_dir)
 
 
-def data_path(filename, raise_missing=True, cache_dir=None):
-    # type: (str, bool, Optional[str]) -> str
+def data_path(
+    filename: str, raise_missing: bool = True, cache_dir: Optional[str] = None
+) -> str:
     if remote_files.is_known_remote(filename):
         return remote_files.remote_file(filename, raise_missing=raise_missing)
 
@@ -63,8 +63,7 @@ def data_path(filename, raise_missing=True, cache_dir=None):
     return str(filepath)
 
 
-def download_all(cache_dir=None):
-    # type: (Optional[str]) -> None
+def download_all(cache_dir: Optional[str] = None) -> None:
     local_dir = _cache_path(cache_dir)
 
     with tempfile.TemporaryFile() as f:
