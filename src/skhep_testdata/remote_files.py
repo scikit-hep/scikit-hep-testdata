3da59d04eb03c207c50bdc78a5e0f33c0e85b1a1
Andres Rios Tascon <ariostas@gmail.com>
2024-11-18 11:57:34 -0500

    chore: drop python 3.8 and test 3.13 (#163)
    * Drop Python 3.8 and test 3.13
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Pre-commit fixes
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fixed mypy
    
    * Added pass job
    
    * added codecov token to action
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/skhep_testdata/remote_files.py
+++ src/skhep_testdata/remote_files.py
@@ -3,17 +3,12 @@ from __future__ import annotations
 import errno
 import logging
 import os
-import sys
 import tarfile
+from importlib import resources
 from typing import ClassVar
 from urllib.request import urlretrieve
 
 import yaml
-
-if sys.version_info < (3, 9):
-    import importlib_resources as resources  # pragma: no cover
-else:
-    from importlib import resources
 
 _default_data_dir = os.path.realpath(os.path.dirname(__file__))
 
